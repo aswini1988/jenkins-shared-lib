3c26a5e5543a59e8fb7a800ef367be1768167f7a
aswparid <aparida4@sapient.com>
2019-07-11 13:44:29 +0100

    write shared lib

---
+++ src/com/jenkins/Build.groovy
@@ -0,0 +1,12 @@
+package com.jenkins
+//This is to build the docker image.
+def build() {
+    def IMAGENAME = 'node-web-app'
+    def dockerfilePATH = './Dockerfile'
+    def REGISTRY_NAME = 'aswini1988'
+    println "Preparing docker image..."
+
+    def tag = 'node-web-app'
+    def appName = config.docker.appname
+    sh "docker build --no-cache -f ${dockerfilePATH} -t ${REGISTRY_NAME}/${IMAGENAME} ."
+}
